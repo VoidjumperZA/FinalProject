8fc1b13e55a1e5588963ed169887b88d0fa2114d
Marian Banas <majobanas@gmail.com>
2017-05-02 13:05:12 +0200

    first mechanic - prototype

---
+++ Final Project/Assets/majoUpload/scripts/basic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic : MonoBehaviour {
+    [SerializeField] private boat _boat;
+    [SerializeField] private Camera _cam;
+    private RaycastHit _hitInfo;
+
+    private bool _selectedBoat = false;
+	void Start () {
+		
+	}
+	void Update () {
+        SelectBoat();
+        MoveBoat2D();
+        DeselectBoat();
+    }
+    private void SelectBoat()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), gameObject.transform.forward), out _hitInfo))
+            {
+                _selectedBoat = _hitInfo.collider.gameObject.CompareTag("Boat") ? true : false;
+            }
+        }
+    }
+    private void MoveBoat2D()
+    {
+        if (Input.GetMouseButton(0) && _selectedBoat)
+        {
+            if (Physics.Raycast(new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), gameObject.transform.forward), out _hitInfo))
+            {
+                _boat.SetDestination(new Vector3(_hitInfo.point.x, _boat.gameObject.transform.position.y, _boat.gameObject.transform.position.z));
+            }
+        }
+    }
+    private void DeselectBoat()
+    {
+        if (Input.GetMouseButtonUp(0) && _selectedBoat)
+        {
+            _selectedBoat = false;
+        }
+    }
+}
