a98787512251abac58d81eb0991b3c42803f20e3
Marian Banas <majobanas@gmail.com>
2017-05-02 14:26:29 +0200

    hook and a boat movement - prototype

---
+++ Final Project/Assets/scripts/boat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour {
+    [SerializeField]
+    private float _slideSpeed;
+    private Vector3 _destination;
+	// Use this for initialization
+	void Start () {
+        _destination = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((_destination - gameObject.transform.position).magnitude > _slideSpeed)
+        {
+            gameObject.transform.Translate((_destination - gameObject.transform.position).normalized * _slideSpeed);
+        }
+	}
+
+    public void SetDestination(Vector3 pDestination)
+    {
+        _destination = pDestination;
+    }
+}
---
+++ Final Project/Assets/scripts/hook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hook : MonoBehaviour {
+
+    [SerializeField]
+    private float _slideSpeed;
+    private Vector3 _destination;
+    // Use this for initialization
+    void Start()
+    {
+        _destination = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_destination != Vector3.zero && (_destination - gameObject.transform.position).magnitude > _slideSpeed)
+        {
+            gameObject.transform.Translate((_destination - gameObject.transform.position).normalized * _slideSpeed);
+        }
+    }
+
+    public void SetDestination(Vector3 pDestination)
+    {
+        _destination = pDestination;
+    }
+
+    public void Appear()
+    {
+        gameObject.transform.localPosition = new Vector3(0, -1, 0);
+    }
+    public void Disappear()
+    {
+        gameObject.transform.localPosition = Vector3.zero;
+    }
+}
