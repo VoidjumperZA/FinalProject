ec7300389f4e66fccb307d3ddd3c16af13232e3f
Josh van Asten (ACER 940M) <joshmathew.va@gmail.com>
2017-05-02 17:25:33 +0200

    + Added a MainMenuManager to control the menu scene + Added a menu scene + Added a CurrentBounty script, for tracking and setting the fish you must hunt + Added the InputTimer script, which needs to be attached to the manager. It counts down and will load the menu scene when it reaches zero. Call ResetClock when you get input.

---
+++ Final Project/Assets/Scripts/InputTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float timeUntilReturnToMenu = 10.0f;
+    private float timeLeft;
+	// Use this for initialization
+	void Start () {
+        timeLeft = timeUntilReturnToMenu;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {        
+        if (timeLeft < 0)
+        {
+            Debug.Log("Time up");
+        }
+        else
+        {
+            timeLeft -= Time.deltaTime;
+            Debug.Log("time: " + timeLeft);
+        }
+	}
+
+    public void ResetClock()
+    {
+        timeLeft = timeUntilReturnToMenu;
+    }
+}
