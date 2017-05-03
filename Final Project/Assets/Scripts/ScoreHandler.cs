385996e5ed85eb32fcda6b6ca17727c9476795f5
Josh van Asten (ACER 940M) <joshmathew.va@gmail.com>
2017-05-03 16:55:03 +0200

    + Added comments to the ScoreHandler and TempFishSpawn scripts

--- Final Project/Assets/Scripts/ScoreHandler.cs
+++ Final Project/Assets/Scripts/ScoreHandler.cs
@@ -5,23 +5,23 @@ using UnityEngine.UI;
 
 public class ScoreHandler : MonoBehaviour {
     [SerializeField]
-    private GameObject scoreUI;
+    private GameObject scoreUI; //the appearing score ui
     [SerializeField]
-    private Text totalScore;
+    private Text totalScore; //the counter listing our total score
     [SerializeField]
-    Color flashColour;
+    Color flashColour;  //which colour the text flashes when it updates
     [SerializeField]
-    float colourFlashTime;
+    float colourFlashTime; //how long does it flash that colour
     [SerializeField]
-    private GameObject UISpawnPosition;
+    private GameObject UISpawnPosition; //where are we spawning that ui
     [SerializeField]
-    float minimumUIScale;
+    float minimumUIScale; //our size is random, what is the minimum bound for scaling
     [SerializeField]
-    float maximumUIScale;
+    float maximumUIScale; //maximum bound for scaling
     [SerializeField]
-    float UIRotationAngle;
+    float UIRotationAngle; //rotating our ui a little for affect
     private Transform UIPosition;
-    private int playerCurrentScore;
+    private int playerCurrentScore; 
     private float timeColourHasBeenFlashing;
     private Color originalTotalScoreColour;
     private bool colourFlashing;
@@ -38,10 +38,11 @@ public class ScoreHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //Temporarily to simulate hitting different fish types
         if (Input.GetKeyDown(KeyCode.W))
         {
             AddScore(10, true);
-
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -52,12 +53,16 @@ public class ScoreHandler : MonoBehaviour {
             AddScore(500, true);
         }
 
+        //if our colour is in flash mode
         if (colourFlashing == true)
         {
+            //count down
             timeColourHasBeenFlashing -= Time.deltaTime;
+            //if time is up
             if (timeColourHasBeenFlashing <= 0)
             {
                 colourFlashing = false;
+                //reset the text back to it's original colour
                 totalScore.color = originalTotalScoreColour;
             }
         }
@@ -67,12 +72,20 @@ public class ScoreHandler : MonoBehaviour {
     {
         return playerCurrentScore;
     }
-
+    /// <summary>
+    /// Set score will completely overwrite the current score, not increment it. Please use AddScore() for that.
+    /// </summary>
+    /// <param name="pNewOverridingScore"></param>
     public void SetScore(int pNewOverridingScore)
     {
         playerCurrentScore = pNewOverridingScore;
     }
 
+    /// <summary>
+    /// Increments our current score. The boolean controls whether UI appears or not.
+    /// </summary>
+    /// <param name="pAddedScore"></param>
+    /// <param name="pCreatUIAnnouncement"></param>
     public void AddScore(int pAddedScore, bool pCreatUIAnnouncement)
     {
         playerCurrentScore += pAddedScore;
@@ -87,10 +100,16 @@ public class ScoreHandler : MonoBehaviour {
         colourFlashing = true;
         totalScore.color = flashColour;
     }
+
+    //Instantiate a UI instance
     private void createScoreUI(int pScore)
     {
         GameObject newScoreInstance = Instantiate(scoreUI, UIPosition);
+
+        //Activate it because our instantiated object is in world, but deactivated. 
         newScoreInstance.SetActive(true);
+
+        //create our random angles and scale and send it to UI
         float angle = Random.Range(-UIRotationAngle, UIRotationAngle);
         float scale = Random.Range(minimumUIScale, maximumUIScale);
         newScoreInstance.GetComponent<ScoreUIAnimation>().SetSpawnParametres(angle, scale);
