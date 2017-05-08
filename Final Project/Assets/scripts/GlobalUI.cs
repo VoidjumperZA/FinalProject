2b6abfe33a4cb745a8b00b803655bbab506b3846
BlancaCelma <blancacelma@gmail.com>
2017-05-08 16:55:29 +0200

    Added ReelUpButton image and Button. Added tags to the buttons. Modified GlobalUI.

--- Final Project/Assets/scripts/GlobalUI.cs
+++ Final Project/Assets/scripts/GlobalUI.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlobalUI : MonoBehaviour {
+
+    private Button _deployHookButton;
+    private Button _reelUpHook;
+    private Button _radarButton;
 
 	// Use this for initialization
 	void Start () {
@@ -18,5 +23,8 @@ public class GlobalUI : MonoBehaviour {
     {
         boat b = GameObject.FindGameObjectWithTag("Boat").GetComponent<boat>();
         b.EnableFishing();
+
     }
+
+    private void DisableButton(Button buttonToDisable) { button1.interactable = false; }
 }
