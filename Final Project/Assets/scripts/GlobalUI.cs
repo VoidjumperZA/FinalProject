cb9f269996034cd6c0b5fb902cb186c9b996fe67
BlancaCelma <blancacelma@gmail.com>
2017-05-08 19:01:09 +0200

    Added radar button and uncomented code to make it work. UI Manager is done.

--- Final Project/Assets/scripts/GlobalUI.cs
+++ Final Project/Assets/scripts/GlobalUI.cs
@@ -7,14 +7,14 @@ public class GlobalUI : MonoBehaviour {
 
     [SerializeField] private Button _deployHookButton;
     [SerializeField] private Button _reelUpHook;
-    //[SerializeField] private Button _radarButton;
+    [SerializeField] private Button _radarButton;
 
     void Start ()
     {
         //Warnings
         if (!_deployHookButton) Debug.Log("Warning: You need to assign DeployHookButton to GlobalUI.");
         if (!_reelUpHook) Debug.Log("Warning: You need to assign ReelUpButton to GlobalUI.");
-        //if (!_radarButton) Debug.Log("Warning: You need to assign RadarButton to GlobalUI.");
+        if (!_radarButton) Debug.Log("Warning: You need to assign RadarButton to GlobalUI.");
 
         _reelUpHook.gameObject.SetActive(false);
     }
@@ -26,7 +26,7 @@ public class GlobalUI : MonoBehaviour {
         b.EnableFishing();
 
         _deployHookButton.gameObject.SetActive(false);
-        //_radarButton.gameObject.SetActive(false);
+        _radarButton.gameObject.SetActive(false);
         _reelUpHook.gameObject.SetActive(true);
    
 
@@ -42,17 +42,13 @@ public class GlobalUI : MonoBehaviour {
     {
         hook h = GameObject.FindGameObjectWithTag("Hook").GetComponent<hook>();
         h.ReelUpTheHook();
-        //SwitchHookButtons();
     }
 
     public void SwitchHookButtons()
     {
-       
         _reelUpHook.gameObject.SetActive(false);
         _deployHookButton.gameObject.SetActive(true);
-        //_radarButton.gameObject.SetActive(true);
-        
-
+        _radarButton.gameObject.SetActive(true);    
     }
 
     private void DisableButton(Button buttonToDisable)
