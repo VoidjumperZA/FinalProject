365669a49d6d92de2cee117ad59e11335049d3c5
BlancaCelma <blancacelma@gmail.com>
2017-05-18 19:59:37 +0200

    Trying to find the problem with the jellyfish...

--- Final Project/Assets/Scripts/Jellyfish/JellyfishSpawn.cs
+++ Final Project/Assets/Scripts/Jellyfish/JellyfishSpawn.cs
@@ -14,21 +14,17 @@ public class JellyfishSpawn : MonoBehaviour {
     private float _spawnCooldown;
     private float _spawnCounter;
 
-    private float _jellyfishZoneLeft;
-    private float _jellyfishZoneRight;
-    private float _jellyfishZoneUp;
-    private float _jellyfishZoneDown;
+    private static float _jellyfishZoneLeft;
+    private static float _jellyfishZoneRight;
+    private static float _jellyfishZoneUp;
+    private static float _jellyfishZoneDown;
 
     // Use this for initialization
     void Start ()
     {
         _numJellyfish = 0;
         _spawnCounter = 0;
-
-        _jellyfishZoneUp = basic.GetJellyfishZoneUp();
-        _jellyfishZoneDown = basic.GetJellyfishZoneDown();
-        _jellyfishZoneLeft = basic.GetJellyfishZoneLeft();
-        _jellyfishZoneRight = basic.GetJellyfishZoneRight();
+        
 	}
 	
 	// Update is called once per frame
@@ -42,10 +38,33 @@ public class JellyfishSpawn : MonoBehaviour {
             {
                 _numJellyfish += 1;
                 _spawnCounter = 0;
-                Instantiate(_jellyfishPrefab, new Vector3(Random.Range(_jellyfishZoneLeft, _jellyfishZoneRight), Random.Range(_jellyfishZoneDown, _jellyfishZoneUp), 0), gameObject.transform.rotation);
+                Debug.Log("Values zone: ");
+                Debug.Log(_jellyfishZoneLeft);
+                //Instantiate(_jellyfishPrefab, new Vector3(Random.Range(_jellyfishZoneLeft, _jellyfishZoneRight), Random.Range(_jellyfishZoneDown, _jellyfishZoneUp), 0), gameObject.transform.rotation);
+                //Instantiate(_jellyfishPrefab, new Vector3(0, 0, 0), gameObject.transform.rotation);
             }
         }
         
 
+    }
+
+    public static void setJellyfishZoneLeft(float leftBoundarie)
+    {
+        _jellyfishZoneLeft = leftBoundarie;
+    }
+
+    public static void setJellyfishZoneRight(float rightBoundarie)
+    {
+        _jellyfishZoneRight = rightBoundarie;
+    }
+
+    public static void setJellyfishZoneUp(float upBoundarie)
+    {
+        _jellyfishZoneUp = upBoundarie;
+    }
+
+    public static void setJellyfishZoneDown(float downBoundarie)
+    {
+        _jellyfishZoneDown = downBoundarie;
     }
 }
