229df3fc5f66d639ceed4abc22bb2420836c66a6
Josh van Asten (ACER 940M) <joshmathew.va@gmail.com>
2017-05-24 13:48:49 +0200

    + Added a RotatePropeller script to attach to the propeller object (when we get a separated model) that will spin the propeller at the speed the boat is moving o Boat model now rotates independately from the boat object itself o Boat movement and camera focusing should be working * Boat pivot still needs to be centered

--- Final Project/Assets/scripts/Boat/States/MoveBoatState.cs
+++ Final Project/Assets/scripts/Boat/States/MoveBoatState.cs
@@ -10,7 +10,6 @@ public class MoveBoatState : AbstractBoatState {
     private float _velocity;
     private float _rotationLerpSpeed;
 
-    private Vector3 _destination = Vector3.zero;
     private Vector3 lerpTarget;
     private Transform previousCamHolder;
     private float polarity;
@@ -21,6 +20,7 @@ public class MoveBoatState : AbstractBoatState {
     private int targetRot;
     private int rotSpeed;
     private Quaternion targetQua;
+    private GameObject boatModel;
 
     public MoveBoatState(boat pBoat, float pAcceleration, float pMaxVelocity, float pDeceleration, float pRotationLerpSpeed) : base(pBoat)
     {
@@ -33,25 +33,29 @@ public class MoveBoatState : AbstractBoatState {
 
     private void SetUpState()
     {
+        boatModel = GameObject.FindGameObjectWithTag("BoatModel");
+        basic.Camerahandler.SetViewPoint(CameraHandler.CameraFocus.Boat);
         _velocity = 0.0f;
         polarity = 0.0f;
         direction = 1.0f;
         turning = false;
 
-        if (_boat.gameObject.transform.rotation == basic.Boat.GetBoatEndRotations(true))
+        if (boatModel.gameObject.transform.rotation == basic.Boat.GetBoatEndRotations(true))
         {
             direction = 1.0f;
             targetRot = 180;
-            targetQua = basic.Boat.GetBoatEndRotations(false);
+            targetQua = basic.Boat.GetBoatEndRotations(true);
+            Debug.Log("Set Up State. Direction: " + direction + "  |  targetQua: Right");
         }
         else
         {
             direction = -1.0f;
             targetRot = -180;
-            targetQua = basic.Boat.GetBoatEndRotations(true);
+            targetQua = basic.Boat.GetBoatEndRotations(false);
+            Debug.Log("Set Up State. Direction: " + direction + "  |  targetQua: Left");
         }
         currentRot = 0;
-        rotSpeed = 1;
+        rotSpeed = GameObject.Find("Manager").GetComponent<GameplayValues>().GetBoatRotationSpeed();
     }
 
     public override void Start()
@@ -64,15 +68,27 @@ public class MoveBoatState : AbstractBoatState {
             basic.GlobalUI.InTutorial = false;
         }
     }
+
+    public float GetBoatVelocity()
+    {
+        return _velocity;
+    }
     public override void Update()
     {
         setPolarity();
+
         //Debug.Log("Direction: " + direction);
         if (!MoveToDestination() && turning == false)
         {
             //Debug.Log("Switching to another state");
             basic.Hook.SetState(hook.HookState.None);
             _boat.SetState(boat.BoatState.Stationary);          
+        }
+
+        if (turning == false)
+        {
+        Debug.Log("Polarity: " + polarity + "\t|\tDirection: " + direction);
+
         }
     }
     private void setPolarity()
@@ -94,13 +110,9 @@ public class MoveBoatState : AbstractBoatState {
     {
         direction = polarity;
         prepareToRotate();
-        Debug.Log("Direction: " + direction);
+        //Debug.Log("Direction: " + direction);
     }
-    private void SetDestination(Vector3 pPosition)
-    {
-        if (!Input.GetMouseButton(0)) return;
-        _destination = new Vector3(pPosition.x - _boat.gameObject.transform.position.x, _boat.gameObject.transform.position.y, _boat.gameObject.transform.position.z);
-    }
+
     private bool MoveToDestination()
     {
         //don't move the boat while it is turning as it will then traverse along incorrect axes
@@ -118,7 +130,7 @@ public class MoveBoatState : AbstractBoatState {
                 //decelerate
                 _velocity -= _deceleration;
                 //if we're at zero check where we're being told to turn around
-                if (_velocity <= 0)
+                if (_velocity <= 0 && direction != polarity)
                 {
                     Debug.Log("Two.");
                     matchDirectionToPolarity();
@@ -130,40 +142,43 @@ public class MoveBoatState : AbstractBoatState {
                 _velocity = 0;
             }
 
-            Vector3 differenceVector = _destination - _boat.gameObject.transform.position;
-            Vector2 m = new Vector3(_velocity, 0.0f, 0.0f);
+            Vector2 m = new Vector3(_velocity * direction, 0.0f, 0.0f);
             _boat.gameObject.transform.Translate(m);
         }
         else
         {
             rotate();
         }
-        return (_velocity > 0 || Input.GetMouseButton(0));
+        return (/*_velocity > 0 || */Input.GetMouseButton(0));
         
     }
 
     //
     private void rotate()
     {
+        Debug.Log("Rotating.");
         //currentRot += rotSpeed * (int)Mathf.Sign(targetRot);
-        _boat.gameObject.transform.Rotate(new Vector3(0.0f, rotSpeed /*.* Mathf.Sign(targetRot)*/, 0.0f));
+        //basic.Boat.gameObject.transform.rotation = Quaternion.Slerp(basic.Boat.gameObject.gameObject.transform.rotation, targetQua, _rotationLerpSpeed * Time.deltaTime);
+        boatModel.gameObject.transform.rotation = Quaternion.RotateTowards(boatModel.gameObject.transform.rotation, targetQua, rotSpeed * Time.deltaTime);
+      //  _boat.gameObject.transform.Rotate(new Vector3(0.0f, rotSpeed, 0.0f));
         /*
-        basic.Boat.gameObject.transform.rotation = Quaternion.Slerp(basic.Boat.gameObject.gameObject.transform.rotation, Quaternion.LookRotation(lerpTarget - basic.Boat.gameObject.gameObject.transform.position), _rotationLerpSpeed * Time.deltaTime);
         //slerp to there
 
         //if we are back set our camera back to it's previous owner*/
-        if (/*basic.Boat.gameObject.transform.rotation == Quaternion.LookRotation(lerpTarget)*/ _boat.gameObject.transform.rotation == targetQua)
+        if (/*basic.Boat.gameObject.transform.rotation == Quaternion.LookRotation(lerpTarget)*/ boatModel.gameObject.transform.rotation == targetQua)
         {
             Camera.main.transform.SetParent(previousCamHolder);
 
             //targetQua = targetQua == _boat.GetBoatEndRotations(true) ? _boat.GetBoatEndRotations(false) : _boat.GetBoatEndRotations(true);
             turning = false;
+            Debug.Log("Turning is false");
         }
     }
 
     //
     private void prepareToRotate()
     {
+        Debug.Log("Preparing to rotate.");
         //shitty rotation
         turning = true;
         targetQua = targetQua == _boat.GetBoatEndRotations(true) ? _boat.GetBoatEndRotations(false) : _boat.GetBoatEndRotations(true);
@@ -174,7 +189,6 @@ public class MoveBoatState : AbstractBoatState {
 
     public override void Refresh()
     {
-        _destination = _boat.gameObject.transform.position;
         SetUpState();
     }
     public override boat.BoatState StateType()
