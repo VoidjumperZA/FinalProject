54149aee85ce08fb51b5e746c44cc6e37ab45a3a
Josh van Asten <joshmathew.va@gmail.com>
2017-06-01 03:42:14 +0200

    o Fixed some errors including missing scripts I had to get from an archive

--- Final Project/Assets/scripts/GameTimer.cs
+++ Final Project/Assets/scripts/GameTimer.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameTimer : MonoBehaviour {
+public class GameTimer : MonoBehaviour
+{
 
     private float totalTimePerLevel;
     private float timeLeft;
     private bool counting;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         totalTimePerLevel = 3 * 60;
         timeLeft = totalTimePerLevel;
         counting = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (counting == true)
         {
             timeLeft -= Time.deltaTime;
-            //Debug.Log("Time: " + timeLeft);
             if (timeLeft <= 0)
             {
                 basic.EndGame();
@@ -30,10 +33,41 @@ public class GameTimer : MonoBehaviour {
                 //radar stop pulsing
             }
         }
-	}
+    }
+
+    public float GetTimeLeft()
+    {
+        return timeLeft;
+
+    }
+
+    public string GetFormattedTimeLeftAsString()
+    {
+        int seconds = Mathf.FloorToInt(GetFormattedTimeLeftAsVec2().y);
+        string componentBreaker = ":";
+        if (seconds < 10)
+        {
+            componentBreaker = ":0";
+        }
+        string str = GetFormattedTimeLeftAsVec2().x + componentBreaker + GetFormattedTimeLeftAsVec2().y;
+        return str;
+    }
+
+    /// <summary>
+    /// Returns a formatted time with x as minutes and y as seconds.
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 GetFormattedTimeLeftAsVec2()
+    {
+        float minutes = Mathf.FloorToInt(timeLeft / 60);
+        float seconds = Mathf.Floor(timeLeft % 60);
+        Vector2 time = new Vector2(minutes, seconds);
+        return time;
+    }
 
     public void BeginCountdown()
     {
         counting = true;
     }
+
 }
