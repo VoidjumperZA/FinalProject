7fb366b861c93d23dc661a2009e106d2216fa0e0
MSI\Darragh <Dhayes1.wex@Gmail.com>
2017-06-01 13:42:02 +0200

    no message

--- Final Project/Assets/scripts/Hook/hook.cs
+++ Final Project/Assets/scripts/Hook/hook.cs
@@ -39,7 +39,7 @@ public class hook : general
     private Vector3 _velocity;
     // X Velocity damping
 
-
+    public ParticleSystem JellyAttackEffect2;
     public Transform HookTip;
 	public ParticleSystem JellyAttackEffect;
 
@@ -99,13 +99,31 @@ public class hook : general
 	public void EnableJellyAttackEffect() 
 	{
 		StartCoroutine(JellyAttackCoroutine());
-	}
+        StartCoroutine(JellyAttackCoroutine2());
+    }
 
 	private IEnumerator JellyAttackCoroutine()
 	{
 		JellyAttackEffect.gameObject.SetActive (true);
+        
 
-		yield return new WaitForSeconds (0.95f);
-		JellyAttackEffect.gameObject.SetActive (false);
-	}
+        yield return new WaitForSeconds (0.95f);
+        
+        JellyAttackEffect.gameObject.SetActive (false);
+        
+
+    }
+    private IEnumerator JellyAttackCoroutine2()
+    {
+        JellyAttackEffect2.gameObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, (basic.Boat.transform.position - JellyAttackEffect2.gameObject.transform.position).normalized);
+        JellyAttackEffect2.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(0.95f);
+
+        
+        JellyAttackEffect2.gameObject.SetActive(false);
+
+    }
+
+
 }
